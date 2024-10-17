0e9db39058633db3709edc46cfe064498be2fa85
obsidian <obsidian0215@gmail.com>
2024-10-17 19:26:48 +0800

    fix bug

--- criu/include/image.h
+++ criu/include/image.h
@@ -149,9 +149,8 @@ extern off_t img_raw_size(struct cr_img *img);
 extern int open_image_dir(char *dir, int mode);
 extern void close_image_dir(void);
 
-// [Obsidian0215]for dirty-map-dir
-extern int open_dirty_map_dir(char *dir);
-extern void close_dirty_map_dir(void);
+// [Obsidian0215]test dirty-map-dir
+extern int test_dirty_map_dir(char *dir);
 /*
  * Return -1 -- parent symlink points to invalid target
  * Return 0 && pfd < 0 -- parent symlink does not exist
