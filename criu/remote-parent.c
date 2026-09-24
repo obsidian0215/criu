f319d4170b87d11fceefa48b274bee03aa358c35
YIFAN LU <luyifan20@mails.ucas.edu.cn>
2026-09-25 01:51:56 +0900

    remote-parent: reuse the pagemap image format

--- criu/remote-parent.c
+++ criu/remote-parent.c
@@ -8,33 +8,22 @@
 #include <unistd.h>
 
 #include "cr_options.h"
+#include "image-desc.h"
 #include "image.h"
 #include "log.h"
+#include "magic.h"
 #include "page-xfer.h"
+#include "protobuf.h"
 #include "remote-parent.h"
 #include "servicefd.h"
 #include "util.h"
-
-#define REMOTE_PARENT_MAGIC   0x52504352U
-#define REMOTE_PARENT_VERSION 1U
+#include "images/pagemap.pb-c.h"
 
 /*
- * Source-side metadata used between pre-dump and final dump.
- * It is not part of the restore image ABI.
+ * The source-side file is a regular CRIU pagemap image. It contains no page
+ * payload, so pages_id is zero and the file is used only to validate parent
+ * ranges during the later local dump.
  */
-struct remote_parent_disk_header {
-	uint32_t magic;
-	uint32_t version;
-	uint32_t fd_type;
-	uint32_t reserved;
-	uint64_t img_id;
-};
-
-struct remote_parent_disk_range {
-	uint64_t vaddr;
-	uint64_t nr_pages;
-};
-
 struct remote_parent_range {
 	unsigned long start;
 	unsigned long end;
@@ -43,18 +32,18 @@ struct remote_parent_range {
 struct remote_parent_coverage {
 	struct remote_parent_range *ranges;
 	size_t nr_ranges;
+	size_t capacity;
 };
 
 struct remote_parent_writer {
-	int fd;
+	struct cr_img *image;
 	int dirfd;
 	char tmp_name[96];
 	char final_name[80];
-	bool have_pending;
 	bool published;
 	bool tmp_created;
-	uint64_t pending_vaddr;
-	uint64_t pending_nr_pages;
+	bool have_entry;
+	uint64_t last_end;
 	struct remote_parent_writer *next;
 };
 
@@ -83,35 +72,27 @@ static int coverage_name(char *buf, size_t size, int fd_type, unsigned long img_
 	return 1;
 }
 
-static int writer_flush_pending(struct remote_parent_writer *writer)
+static int write_pagemap_header(struct cr_img *image, int fd_type)
 {
-	struct remote_parent_disk_range range;
+	PagemapHead head = PAGEMAP_HEAD__INIT;
+	u32 magic = IMG_COMMON_MAGIC;
 
-	if (!writer->have_pending)
-		return 0;
-
-	range.vaddr = writer->pending_vaddr;
-	range.nr_pages = writer->pending_nr_pages;
-	if (write_all(writer->fd, &range, sizeof(range)) != (ssize_t)sizeof(range)) {
-		pr_perror("Unable to write remote-parent coverage");
-		writer_error = true;
+	if (write_img(image, &magic))
 		return -1;
-	}
+	magic = imgset_template[fd_type].magic;
+	if (write_img(image, &magic))
+		return -1;
 
-	writer->have_pending = false;
-	return 0;
+	/* No pages image exists on the source side. */
+	head.pages_id = 0;
+	return pb_write_one(image, &head, PB_PAGEMAP_HEAD);
 }
 
 int remote_parent_writer_open(int fd_type, unsigned long img_id, struct remote_parent_writer **out)
 {
-	struct remote_parent_disk_header header = {
-		.magic = REMOTE_PARENT_MAGIC,
-		.version = REMOTE_PARENT_VERSION,
-		.fd_type = fd_type,
-		.img_id = img_id,
-	};
 	struct remote_parent_writer *writer;
 	int base_fd;
+	int fd = -1;
 	int ret;
 
 	*out = NULL;
@@ -123,8 +104,6 @@ int remote_parent_writer_open(int fd_type, unsigned long img_id, struct remote_p
 		writer_error = true;
 		return -1;
 	}
-
-	writer->fd = -1;
 	writer->dirfd = -1;
 
 	ret = coverage_name(writer->final_name, sizeof(writer->final_name), fd_type, img_id);
@@ -149,17 +128,21 @@ int remote_parent_writer_open(int fd_type, unsigned long img_id, struct remote_p
 		goto err;
 	}
 
-	writer->fd = openat(writer->dirfd, writer->tmp_name,
-			    O_CREAT | O_EXCL | O_WRONLY | O_CLOEXEC | O_NOFOLLOW, 0600);
-	if (writer->fd < 0) {
-		pr_perror("Unable to create remote-parent coverage %s", writer->tmp_name);
+	fd = openat(writer->dirfd, writer->tmp_name,
+		    O_CREAT | O_EXCL | O_WRONLY | O_CLOEXEC | O_NOFOLLOW, 0600);
+	if (fd < 0) {
+		pr_perror("Unable to create remote-parent pagemap %s", writer->tmp_name);
 		goto err;
 	}
-
 	writer->tmp_created = true;
 
-	if (write_all(writer->fd, &header, sizeof(header)) != (ssize_t)sizeof(header)) {
-		pr_perror("Unable to write remote-parent coverage header");
+	writer->image = img_from_fd(fd);
+	if (!writer->image)
+		goto err;
+	fd = -1;
+
+	if (write_pagemap_header(writer->image, fd_type)) {
+		pr_err("Unable to write remote-parent pagemap header\n");
 		goto err;
 	}
 
@@ -171,8 +154,10 @@ int remote_parent_writer_open(int fd_type, unsigned long img_id, struct remote_p
 err:
 	/* An earlier writer must not be published after this open failed. */
 	writer_error = true;
-	if (writer->fd >= 0)
-		close(writer->fd);
+	if (writer->image)
+		close_image(writer->image);
+	else if (fd >= 0)
+		close(fd);
 	if (writer->dirfd >= 0) {
 		if (writer->tmp_created)
 			unlinkat(writer->dirfd, writer->tmp_name, 0);
@@ -184,45 +169,43 @@ err:
 
 int remote_parent_writer_record(struct remote_parent_writer *writer, const struct iovec *iov, u32 flags)
 {
+	PagemapEntry entry = PAGEMAP_ENTRY__INIT;
 	uint64_t vaddr;
 	uint64_t nr_pages;
 	uint64_t end;
-	uint64_t pending_end;
 
 	if (!writer || !(flags & (PE_PRESENT | PE_PARENT)))
 		return 0;
 
 	if (!iov->iov_len || iov->iov_len % PAGE_SIZE) {
-		pr_err("Invalid remote-parent coverage length %zu\n", iov->iov_len);
+		pr_err("Invalid remote-parent pagemap length %zu\n", iov->iov_len);
 		goto err;
 	}
 
 	vaddr = (uintptr_t)iov->iov_base;
 	nr_pages = iov->iov_len / PAGE_SIZE;
 	if ((vaddr & (PAGE_SIZE - 1)) || nr_pages > (UINT64_MAX - vaddr) / PAGE_SIZE) {
-		pr_err("Invalid remote-parent coverage range at %#" PRIx64 "\n", vaddr);
+		pr_err("Invalid remote-parent pagemap range at %#" PRIx64 "\n", vaddr);
 		goto err;
 	}
 	end = vaddr + nr_pages * PAGE_SIZE;
-
-	if (writer->have_pending) {
-		pending_end = writer->pending_vaddr + writer->pending_nr_pages * PAGE_SIZE;
-		if (vaddr < writer->pending_vaddr) {
-			pr_err("Out-of-order remote-parent coverage at %#" PRIx64 "\n", vaddr);
-			goto err;
-		}
-		if (vaddr <= pending_end) {
-			if (end > pending_end)
-				writer->pending_nr_pages = (end - writer->pending_vaddr) / PAGE_SIZE;
-			return 0;
-		}
-		if (writer_flush_pending(writer))
-			return -1;
+	if (writer->have_entry && vaddr < writer->last_end) {
+		pr_err("Overlapping or out-of-order remote-parent pagemap at %#" PRIx64 "\n", vaddr);
+		goto err;
 	}
 
-	writer->pending_vaddr = vaddr;
-	writer->pending_nr_pages = nr_pages;
-	writer->have_pending = true;
+	entry.vaddr = vaddr;
+	entry.nr_pages = nr_pages;
+	entry.has_nr_pages = true;
+	entry.flags = flags;
+	entry.has_flags = true;
+	if (pb_write_one(writer->image, &entry, PB_PAGEMAP) < 0) {
+		pr_err("Unable to write remote-parent pagemap entry\n");
+		goto err;
+	}
+
+	writer->have_entry = true;
+	writer->last_end = end;
 	return 0;
 
 err:
@@ -232,15 +215,11 @@ err:
 
 void remote_parent_writer_close(struct remote_parent_writer *writer)
 {
-	if (!writer || writer->fd < 0)
+	if (!writer || !writer->image)
 		return;
 
-	writer_flush_pending(writer);
-	if (close(writer->fd)) {
-		pr_perror("Unable to close remote-parent coverage");
-		writer_error = true;
-	}
-	writer->fd = -1;
+	close_image(writer->image);
+	writer->image = NULL;
 }
 
 static int unlink_coverage(int dirfd, const char *name)
@@ -248,7 +227,7 @@ static int unlink_coverage(int dirfd, const char *name)
 	if (!unlinkat(dirfd, name, 0) || errno == ENOENT)
 		return 0;
 
-	pr_perror("Unable to remove remote-parent coverage %s", name);
+	pr_perror("Unable to remove remote-parent pagemap %s", name);
 	return -1;
 }
 
@@ -260,10 +239,8 @@ static int writer_cleanup_all(bool remove_published)
 	while (writer) {
 		struct remote_parent_writer *next = writer->next;
 
-		if (writer->fd >= 0 && close(writer->fd)) {
-			pr_perror("Unable to close remote-parent coverage");
-			ret = -1;
-		}
+		if (writer->image)
+			close_image(writer->image);
 		if (writer->dirfd >= 0) {
 			if (writer->tmp_created && unlink_coverage(writer->dirfd, writer->tmp_name))
 				ret = -1;
@@ -301,7 +278,7 @@ int remote_parent_finish(bool commit)
 
 	for (writer = pending_writers; writer; writer = writer->next) {
 		if (linkat(writer->dirfd, writer->tmp_name, writer->dirfd, writer->final_name, 0)) {
-			pr_perror("Unable to commit remote-parent coverage %s", writer->final_name);
+			pr_perror("Unable to commit remote-parent pagemap %s", writer->final_name);
 			goto rollback;
 		}
 		writer->published = true;
@@ -317,19 +294,17 @@ rollback:
 	return -1;
 }
 
-static int coverage_file_open(int dirfd, int fd_type, unsigned long img_id, int *fd_out, size_t *nr_ranges)
+static int open_coverage_image(int dirfd, int fd_type, unsigned long img_id, struct cr_img **out)
 {
-	struct remote_parent_disk_header header;
+	PagemapHead *head = NULL;
+	struct cr_img *image = NULL;
 	struct stat st;
-	uintmax_t payload;
-	uintmax_t count;
 	char name[80];
+	u32 magic;
 	int fd;
 	int ret;
 
-	*fd_out = -1;
-	*nr_ranges = 0;
-
+	*out = NULL;
 	ret = coverage_name(name, sizeof(name), fd_type, img_id);
 	if (ret <= 0)
 		return ret;
@@ -338,143 +313,173 @@ static int coverage_file_open(int dirfd, int fd_type, unsigned long img_id, int 
 	if (fd < 0) {
 		if (errno == ENOENT)
 			return 0;
-		pr_perror("Unable to open remote-parent coverage %s", name);
+		pr_perror("Unable to open remote-parent pagemap %s", name);
+		return -1;
+	}
+	if (fstat(fd, &st)) {
+		pr_perror("Unable to stat remote-parent pagemap %s", name);
+		close(fd);
+		return -1;
+	}
+	if (!S_ISREG(st.st_mode)) {
+		pr_err("Remote-parent pagemap %s is not a regular file\n", name);
+		close(fd);
 		return -1;
 	}
 
-	if (fstat(fd, &st)) {
-		pr_perror("Unable to stat remote-parent coverage %s", name);
+	image = img_from_fd(fd);
+	if (!image) {
+		close(fd);
+		return -1;
+	}
+
+	if (read_img(image, &magic) < 0 || magic != IMG_COMMON_MAGIC) {
+		pr_err("Remote-parent pagemap %s has invalid common magic\n", name);
 		goto err;
 	}
-	if (!S_ISREG(st.st_mode) || st.st_size < (off_t)sizeof(header)) {
-		pr_err("Malformed remote-parent coverage %s\n", name);
+	if (read_img(image, &magic) < 0 || magic != imgset_template[fd_type].magic) {
+		pr_err("Remote-parent pagemap %s has invalid image magic\n", name);
+		goto err;
+	}
+	if (pb_read_one(image, &head, PB_PAGEMAP_HEAD) < 0) {
+		pr_err("Unable to read remote-parent pagemap header\n");
+		goto err;
+	}
+	if (head->pages_id != 0) {
+		pr_err("Remote-parent pagemap %s unexpectedly refers to a pages image\n", name);
 		goto err;
 	}
 
-	payload = (uintmax_t)(st.st_size - sizeof(header));
-	if (payload % sizeof(struct remote_parent_disk_range)) {
-		pr_err("Malformed remote-parent coverage %s\n", name);
-		goto err;
-	}
-	count = payload / sizeof(struct remote_parent_disk_range);
-	if (count > SIZE_MAX) {
-		pr_err("Remote-parent coverage %s is too large\n", name);
-		goto err;
-	}
-
-	if (read_all(fd, &header, sizeof(header)) != (ssize_t)sizeof(header)) {
-		pr_perror("Unable to read remote-parent coverage header");
-		goto err;
-	}
-	if (header.magic != REMOTE_PARENT_MAGIC || header.version != REMOTE_PARENT_VERSION ||
-	    header.fd_type != (uint32_t)fd_type || header.reserved || header.img_id != (uint64_t)img_id) {
-		pr_err("Remote-parent coverage %s has an incompatible header\n", name);
-		goto err;
-	}
-
-	*fd_out = fd;
-	*nr_ranges = (size_t)count;
+	pagemap_head__free_unpacked(head, NULL);
+	*out = image;
 	return 1;
 
 err:
-	close(fd);
+	if (head)
+		pagemap_head__free_unpacked(head, NULL);
+	close_image(image);
 	return -1;
+}
+
+static void init_compat_entry(PagemapEntry *entry)
+{
+	if (entry->has_in_parent && entry->in_parent)
+		entry->flags |= PE_PARENT;
+	else if (!entry->has_flags)
+		entry->flags = PE_PRESENT;
+
+	if (!entry->has_nr_pages)
+		entry->nr_pages = entry->compat_nr_pages;
+}
+
+static int coverage_add(struct remote_parent_coverage *coverage, uint64_t start, uint64_t end)
+{
+	struct remote_parent_range *last;
+
+	if (coverage->nr_ranges) {
+		last = &coverage->ranges[coverage->nr_ranges - 1];
+		if (start < last->start) {
+			pr_err("Remote-parent pagemap entries are out of order\n");
+			return -1;
+		}
+		if (start <= last->end) {
+			if (end > last->end)
+				last->end = (unsigned long)end;
+			return 0;
+		}
+	}
+
+	if (coverage->nr_ranges == coverage->capacity) {
+		size_t capacity = coverage->capacity ? coverage->capacity * 2 : 16;
+		struct remote_parent_range *ranges;
+
+		if (capacity < coverage->capacity || capacity > SIZE_MAX / sizeof(*ranges)) {
+			pr_err("Remote-parent pagemap is too large\n");
+			return -1;
+		}
+		ranges = xrealloc(coverage->ranges, capacity * sizeof(*ranges));
+		if (!ranges)
+			return -1;
+		coverage->ranges = ranges;
+		coverage->capacity = capacity;
+	}
+
+	coverage->ranges[coverage->nr_ranges++] = (struct remote_parent_range){
+		.start = (unsigned long)start,
+		.end = (unsigned long)end,
+	};
+	return 0;
 }
 
 int remote_parent_coverage_open(int dirfd, int fd_type, unsigned long img_id,
 				struct remote_parent_coverage **out)
 {
-	struct remote_parent_disk_range disk;
 	struct remote_parent_coverage *coverage;
-	size_t nr_disk;
-	size_t i;
-	int fd;
+	struct cr_img *image;
+	PagemapEntry *entry = NULL;
 	int ret;
 
 	*out = NULL;
-	ret = coverage_file_open(dirfd, fd_type, img_id, &fd, &nr_disk);
+	ret = open_coverage_image(dirfd, fd_type, img_id, &image);
 	if (ret <= 0)
 		return ret;
 
 	coverage = xzalloc(sizeof(*coverage));
-	if (!coverage)
-		goto err_fd;
-
-	if (nr_disk > SIZE_MAX / sizeof(*coverage->ranges)) {
-		pr_err("Remote-parent coverage is too large\n");
-		goto err;
-	}
-	if (nr_disk) {
-		coverage->ranges = xmalloc(nr_disk * sizeof(*coverage->ranges));
-		if (!coverage->ranges)
-			goto err;
+	if (!coverage) {
+		close_image(image);
+		return -1;
 	}
 
-	for (i = 0; i < nr_disk; i++) {
-		struct remote_parent_range *last;
+	while ((ret = pb_read_one_eof(image, &entry, PB_PAGEMAP)) > 0) {
 		uint64_t end;
 
-		if (read_all(fd, &disk, sizeof(disk)) != (ssize_t)sizeof(disk)) {
-			pr_perror("Unable to read remote-parent coverage range");
-			goto err;
+		init_compat_entry(entry);
+		if (!entry->nr_pages || (entry->vaddr & (PAGE_SIZE - 1)) || entry->vaddr > ULONG_MAX ||
+		    entry->nr_pages > (UINT64_MAX - entry->vaddr) / PAGE_SIZE) {
+			pr_err("Invalid remote-parent pagemap entry\n");
+			goto err_entry;
 		}
-		if (!disk.nr_pages || (disk.vaddr & (PAGE_SIZE - 1)) || disk.vaddr > ULONG_MAX ||
-		    disk.nr_pages > (UINT64_MAX - disk.vaddr) / PAGE_SIZE) {
-			pr_err("Invalid remote-parent coverage range\n");
-			goto err;
+		if ((entry->flags & (PE_PRESENT | PE_PARENT)) == (PE_PRESENT | PE_PARENT)) {
+			pr_err("Remote-parent pagemap entry is both present and inherited\n");
+			goto err_entry;
 		}
 
-		end = disk.vaddr + disk.nr_pages * PAGE_SIZE;
+		end = entry->vaddr + entry->nr_pages * PAGE_SIZE;
 		if (end > ULONG_MAX) {
-			pr_err("Remote-parent coverage range exceeds address space\n");
-			goto err;
+			pr_err("Remote-parent pagemap entry exceeds address space\n");
+			goto err_entry;
+		}
+		if (entry->flags & (PE_PRESENT | PE_PARENT)) {
+			if (coverage_add(coverage, entry->vaddr, end))
+				goto err_entry;
 		}
 
-		if (!coverage->nr_ranges) {
-			coverage->ranges[coverage->nr_ranges++] = (struct remote_parent_range){
-				.start = (unsigned long)disk.vaddr,
-				.end = (unsigned long)end,
-			};
-			continue;
-		}
-
-		last = &coverage->ranges[coverage->nr_ranges - 1];
-		if (disk.vaddr < last->start) {
-			pr_err("Remote-parent coverage ranges are out of order\n");
-			goto err;
-		}
-		if (disk.vaddr <= last->end) {
-			if (end > last->end)
-				last->end = (unsigned long)end;
-			continue;
-		}
-
-		coverage->ranges[coverage->nr_ranges++] = (struct remote_parent_range){
-			.start = (unsigned long)disk.vaddr,
-			.end = (unsigned long)end,
-		};
+		pagemap_entry__free_unpacked(entry, NULL);
+		entry = NULL;
 	}
+	if (ret < 0)
+		goto err;
 
-	close(fd);
+	close_image(image);
 	*out = coverage;
 	return 1;
 
+err_entry:
+	pagemap_entry__free_unpacked(entry, NULL);
 err:
+	close_image(image);
 	remote_parent_coverage_close(coverage);
-err_fd:
-	close(fd);
 	return -1;
 }
 
 int remote_parent_coverage_exists(int dirfd, int fd_type, unsigned long img_id)
 {
-	size_t nr_ranges;
-	int fd;
+	struct remote_parent_coverage *coverage = NULL;
 	int ret;
 
-	ret = coverage_file_open(dirfd, fd_type, img_id, &fd, &nr_ranges);
+	ret = remote_parent_coverage_open(dirfd, fd_type, img_id, &coverage);
 	if (ret > 0)
-		close(fd);
+		remote_parent_coverage_close(coverage);
 	return ret;
 }
 
